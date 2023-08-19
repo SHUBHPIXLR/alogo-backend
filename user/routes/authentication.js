d155a826214c354edfcda5b6dfbb9e94f94a1129
SHUBHPIXLR <shubham.pixlerit@gmail.com>
2023-08-19 18:38:25 +0530

    mail design changes.

--- user/routes/authentication.js
+++ user/routes/authentication.js
@@ -201,9 +201,9 @@ const handleVerification = async (email, token) => {
       }
   });
   let htmldfs = '<div>'+
-  '<div style="background-color: #FFED00;width: 100%; text-align: center; padding:15px 0px;">Click Here to '+
+  '<div style="background-color: #FFED00;width: 100%; text-align: center; padding:15px 0px; font-weight: 600;">Click Here to '+
   // '<img style="background-color: #26377C;" src="http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g" alt="">'+
-    '<a href = "' + clientURL +'/' + token + '" style="color: #ffffff; font-weight: 600;">Change Password</a>'+  
+    '<a href = "' + clientURL +'/' + token + '" style="color: #b8b8b8; font-weight: 600;">Change Password</a>'+  
   '</div>'+
   '<div style="width: 300px; flex-wrap: wrap; margin: 0 -15px;">'+
     
